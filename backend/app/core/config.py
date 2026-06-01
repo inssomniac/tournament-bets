96306fe125e59c36ffeb7c3c8ae5a407c43b8d06
inssomniac <163541538+inssomniac@users.noreply.github.com>
2026-06-01 20:55:30 +0700

    очередные правки

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ from typing import List
 class Settings(BaseSettings):
     DATABASE_URL: str
     BOT_TOKEN: str
-    LAPTU_CHANNEL_ID: int
+    LAPTU_CHANNEL_ID: str = ""
     CHANNEL_INVITE_URL: str = ""   # https://t.me/+XXXX или https://t.me/username
     SECRET_KEY: str
     ADMIN_IDS: str  # "123,456" — парсим в список
